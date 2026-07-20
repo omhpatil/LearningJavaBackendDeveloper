b0800b4ec39e8ff3397792878e431b1bcd3bdc4e
Om Patil <patilom887@gmail.com>
2026-07-20 17:35:35 +0530

    Java 8 feature : StringJoinder

---
+++ Java8Features/StringJoinerDemo.java
@@ -0,0 +1,38 @@
+package Java8Features;
+
+import java.util.StringJoiner;
+
+public class StringJoinerDemo {
+
+    public static void main(String[] args) {
+
+        StringJoiner skills =
+                new StringJoiner(", ", "[", "]");
+
+        skills.add("Java");
+        skills.add("Spring Boot");
+        skills.add("Microservices");
+        skills.add("Kafka");
+
+        System.out.println(skills);
+
+        System.out.println("Length : " + skills.length());
+
+        StringJoiner databases =
+                new StringJoiner(", ");
+
+        databases.add("MySQL");
+        databases.add("MongoDB");
+
+        skills.merge(databases);
+
+        System.out.println(skills);
+
+        StringJoiner empty =
+                new StringJoiner(", ");
+
+        empty.setEmptyValue("No Skills");
+
+        System.out.println(empty);
+    }
+}
