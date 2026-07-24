f4bf64f69af648d46464fdba51852e4598499975
Om Patil <patilom887@gmail.com>
2026-07-24 17:42:05 +0530

    field injection

---
+++ Spring_By_Ashok_IT/A03_Dependency_Injection/Field_Injection/Car.java
@@ -0,0 +1,15 @@
+package Spring_By_Ashok_IT.A03_Dependency_Injection.Field_Injection;
+
+public class Car {
+
+    private Engine engine = new Engine();
+
+    public void drive() {
+        engine.start();
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.drive();
+    }
+}
